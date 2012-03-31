8f4cc5c05e9f7e3bf2de755b6952bb742d40e52c
sirjaren <sirjaren@gmail.com>
2012-03-30 19:43:48 -0500

    Added new argument "--aucdtect,-a".  Code cleanup.
    The new argument, "--aucdtect,-a" uses the program auCDtect to test FLAC
    validity (determine whether FLAC is lossy sourced).  Added configurable
    auCDtect command location under USER CONFIGURATION.  Updated
    documentation.

--- redoFlacs.sh
+++ redoFlacs.sh
@@ -2,7 +2,7 @@
 
 #------------------------------------------------------------
 # Re-compress, Verify, Test, Re-tag, and Clean Up FLAC Files
-#                      Version 0.7.2
+#                      Version 0.8
 #                       sirjaren
 #------------------------------------------------------------
 
@@ -18,9 +18,22 @@
 # GNU General Public License for more details.
 #-----------------------------------------------------------------
 
+# TODO: Make auCDtect skip files that are higher than 16bit/44.1kHz
+# TODO: Use tput to print information using the entire terminal screen
+#       independent of size
+
 #-----------
 # Changelog
 #-----------
+# 0.8   [03/29/12]
+#         --Added new argument (--aucdtect,-a) to allow use of
+#           auCDtect command to test FLAC validity (determine
+#           whether FLAC is lossy sourced)
+#         --Addded configurable auCDtect command location under
+#           USER CONFIGURATION
+#         --Code cleanup
+#           readable)
+#         --Updated documentation
 # 0.7.2 [12/04/11]
 #         --Fixed a bug where finding FLAC files in the current
 #           working directory yielded incorrect shell expansion.
@@ -125,12 +138,20 @@ CORES=2
 # the user's HOME directory.
 ERROR_LOG="$HOME"
 
+# Set where the auCDtect command is located.
+# By default, the script will look in:
+# /usr/local/bin
+AUCDTECT_COMMAND="/usr/local/bin/auCDtect"
+
 ##########################
 #  END OF CONFIGURATION  #
 ##########################
 
 # Version
-VERSION="0.7.2"
+VERSION="0.8"
+
+# Export auCDtect command to allow subshell access
+export AUCDTECT_COMMAND
 
 # Export the tag array using some trickery (BASH doesn't
 # support exporting arrays natively)
@@ -149,11 +170,13 @@ export VERIFY_ERRORS="$ERROR_LOG/FLAC_Verify_Errors $(date "+[%Y-%m-%d %R]")"
 export TEST_ERRORS="$ERROR_LOG/FLAC_Test_Errors $(date "+[%Y-%m-%d %R]")"
 export MD5_ERRORS="$ERROR_LOG/MD5_Signature_Errors $(date "+[%Y-%m-%d %R]")"
 export METADATA_ERRORS="$ERROR_LOG/FLAC_Metadata_Errors $(date "+[%Y-%m-%d %R]")"
+export AUCDTECT_ERRORS="$ERROR_LOG/auCDtect_Errors $(date "+[%Y-%m-%d %R]")"
 
 # Set arguments to false
 # If enabled they will be changed to true
 COMPRESS="false"
 TEST="false"
+AUCDTECT="false"
 MD5CHECK="false"
 PRUNE="false"
 REDO="false"
@@ -163,66 +186,109 @@ REDO="false"
 function title_compress_flac {
 	echo -e " ${BOLD_GREEN}*${NORMAL} Compressing FLAC files with level 8 compression and verifying output"
 }
+
 function title_analyze_tags {
 	echo -e " ${BOLD_GREEN}*${NORMAL} Analyzing FLAC Tags"
 }
+
 function title_setting_tags {
 	echo -e " ${BOLD_GREEN}*${NORMAL} Setting new FLAC Tags"
 }
+
 function title_testing_flac {
 	echo -e " ${BOLD_GREEN}*${NORMAL} Testing the integrity of each FLAC file"
 }
+
+function title_aucdtect_flac {
+	echo -e " ${BOLD_GREEN}*${NORMAL} Validating FLAC is not lossy sourced"
+}
+
 function title_md5check_flac {
 	echo -e " ${BOLD_GREEN}*${NORMAL} Verifying the MD5 Signature in each FLAC file"
 }
+
 function title_prune_flac {
 	echo -e " ${BOLD_GREEN}*${NORMAL} Removing the SEEKTABLE and PADDING block from each FLAC file"
 }
+
 # Error messages
 function no_flacs {
 	echo -e " $BOLD_RED*${NORMAL} There are not any FLAC files to process"
 }
+
 # Information relating to currently running tasks
-# Must be exported in order for multithreading
 function print_compressing_flac {
-	printf "\r%75s${YELLOW}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s" "[" "Compressing FLAC" "]" "     " "*" " $(basename "$i" | cut -c1-65)"
+	printf "\r%75s${YELLOW}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s" \
+	"[" "Compressing FLAC" "]" "     " "*" " $(basename "$i" | cut -c1-65)"
 }
-export -f print_compressing_flac
+
 function print_testing_flac {
-	printf "\r%75s${YELLOW}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s" "[" "Testing FLAC" "]" "     " "*" " $(basename "$i" | cut -c1-65)"
+	printf "\r%75s${YELLOW}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s" \
+	"[" "Testing FLAC" "]" "     " "*" " $(basename "$i" | cut -c1-65)"
 }
-export -f print_testing_flac
+
 function print_failed_flac {
-	printf "\r%75s${BOLD_RED}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s\n" "[" "FAILED" "]          " "     " "*" " $(basename "$i" | cut -c1-65)"
+	printf "\r%75s${BOLD_RED}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s\n" \
+	"[" "FAILED" "]          " "     " "*" " $(basename "$i" | cut -c1-65)"
 }
-export -f print_failed_flac
+
 function print_checking_md5 {
-	printf "\r%75s${YELLOW}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s" "[" "Checking MD5" "]" "     " "*" " $(basename "$i" | cut -c1-65)"
+	printf "\r%75s${YELLOW}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s" \
+	"[" "Checking MD5" "]" "     " "*" " $(basename "$i" | cut -c1-65)"
 }
-export -f print_checking_md5
+
 function print_ok_flac {
-	printf "\r%75s${BOLD_GREEN}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s\n" "[" "OK" "]              " "     " "*" " $(basename "$i" | cut -c1-65)"
+	printf "\r%75s${BOLD_GREEN}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s\n" \
+	"[" "OK" "]              " "     " "*" " $(basename "$i" | cut -c1-65)"
 }
-export -f print_ok_flac
+
+function print_aucdtect_flac {
+	printf "\r%75s${YELLOW}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s\n" \
+	"[" "Validating FLAC" "]  " "     " "*" " $(basename "$i" | cut -c1-65)"
+}
+
+function print_aucdtect_issue {
+	printf "\r%75s${YELLOW}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s\n" \
+	"[" "ISSUE" "]           " "     " "*" " $(basename "$i" | cut -c1-65)"
+}
+
 function print_done_flac {
-    printf "\r%75s${BOLD_GREEN}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s\n" "[" "DONE" "]            " "     " "*" " $(basename "$i" | cut -c1-65)"
+    printf "\r%75s${BOLD_GREEN}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s\n" \
+	"[" "DONE" "]            " "     " "*" " $(basename "$i" | cut -c1-65)"
 }
-export -f print_done_flac
+
 function print_level_8 {
-	printf "\r%75s${YELLOW}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s\n" "[" "Already At Level 8" "]" "     " "*" " $(basename "$i" | cut -c1-65)"
+	printf "\r%75s${YELLOW}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s\n" \
+	"[" "Already At Level 8" "]" "     " "*" " $(basename "$i" | cut -c1-65)"
 }
-export -f print_level_8
+
 function print_analyzing_tags {
-	printf "\r%75s${YELLOW}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s" "[" "Analyzing Tags" "]" "     " "*" " $(basename "$i"     | cut -c1-65)"
+	printf "\r%75s${YELLOW}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s" \
+	"[" "Analyzing Tags" "]" "     " "*" " $(basename "$i"     | cut -c1-65)"
 }
-export -f print_analyzing_tags 
+
 function print_setting_tags {
-	printf "\r%75s${YELLOW}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s" "[" "Setting Tags" "]" "     " "*" " $(basename "$i"     | cut -c1-65)"
+	printf "\r%75s${YELLOW}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s" \
+	"[" "Setting Tags" "]" "     " "*" " $(basename "$i"     | cut -c1-65)"
 }
-export -f print_setting_tags
+
 function print_prune_flac {
-	printf "\r%75s${YELLOW}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s" "[" "Pruning Metadata" "]" "     " "*" " $(basename "$i"     | cut -c1-65)"
+	printf "\r%75s${YELLOW}%s${NORMAL}%s\r%s${YELLOW}%s${NORMAL}%s" \
+	"[" "Pruning Metadata" "]" "     " "*" " $(basename "$i"     | cut -c1-65)"
 }
+
+# Export all the above functions for subshell access
+export -f print_compressing_flac
+export -f print_testing_flac
+export -f print_failed_flac
+export -f print_checking_md5
+export -f print_ok_flac
+export -f print_aucdtect_flac
+export -f print_aucdtect_issue
+export -f print_done_flac
+export -f print_level_8
+export -f print_analyzing_tags 
+export -f print_setting_tags
 export -f print_prune_flac
 
 # General abort script to use BASH's trap command on SIGINT
@@ -248,7 +314,7 @@ function countdown_metadata {
 		COUNT=10
 		while [[ $COUNT -gt 1 ]] ; do
 			echo -en "${BOLD_RED}${COUNT}${NORMAL} "
-			sleep 1;
+			sleep 1
 			((COUNT--))
 		done
 	}
@@ -285,8 +351,8 @@ function compress_flacs {
 		echo -e "\n ${BOLD_GREEN}*${NORMAL} Control-C received, removing temporary files and exiting script..."
 		find "$DIRECTORY" -name *.tmp,fl-ac+en\'c -exec rm "{}" \;
 		if [[ -f "$VERIFY_ERRORS" ]] ; then
-        	echo -e "\n ${BOLD_RED}*${NORMAL} Errors found in some FLAC files, please check"
-        	echo -e " ${BOLD_RED}*${NORMAL} \"$VERIFY_ERRORS\" for errors"
+			echo -e "\n ${BOLD_RED}*${NORMAL} Errors found in some FLAC files, please check"
+			echo -e " ${BOLD_RED}*${NORMAL} \"$VERIFY_ERRORS\" for errors"
 		fi
 		exit $?
 	}
@@ -319,10 +385,10 @@ function compress_flacs {
 				ERROR="$((flac -ts "$i") 2>&1)"
 				if [[ ! -z "$ERROR" ]] ; then
 					print_failed_flac
-            		echo -e "[[$i]]\n"  "$ERROR\n" >> "$VERIFY_ERRORS"
-        		else 
+					echo -e "[[$i]]\n"  "$ERROR\n" >> "$VERIFY_ERRORS"
+				else 
 					print_ok_flac
-        		fi
+				fi
 			fi
 		done
 	}
@@ -331,22 +397,22 @@ function compress_flacs {
 	# Run the above function with the configured threads (multithreaded)
 	find "$DIRECTORY" -name *.flac -print0 | xargs -0 -n 1 -P "$CORES" bash -c 'compress_f "$@"' --
 	
-    if [[ -f "$VERIFY_ERRORS" ]] ; then
-        echo -e "\n ${BOLD_RED}*${NORMAL} Errors found in some FLAC files, please check"
-        echo -e " ${BOLD_RED}*${NORMAL} \"$VERIFY_ERRORS\" for errors"
+	if [[ -f "$VERIFY_ERRORS" ]] ; then
+		echo -e "\n ${BOLD_RED}*${NORMAL} Errors found in some FLAC files, please check"
+		echo -e " ${BOLD_RED}*${NORMAL} \"$VERIFY_ERRORS\" for errors"
 		exit 0
-    fi
+	fi
 }
 
 # Test FLAC files
 function test_flacs {
 	title_testing_flac
 
-    FIND_FLACS="$(find "$DIRECTORY" -name "*.flac" -print)"
-    if [[ -z "$FIND_FLACS" ]] ; then
+	FIND_FLACS="$(find "$DIRECTORY" -name "*.flac" -print)"
+	if [[ -z "$FIND_FLACS" ]] ; then
 		no_flacs
 		exit 0
-    fi
+	fi
 
 	# Abort script and check for any errors thus far
 	function test_abort {
@@ -359,9 +425,9 @@ function test_flacs {
 	}
 
 	# Trap SIGINT (Control-C) to abort cleanly
-    trap test_abort SIGINT
+	trap test_abort SIGINT
 
-    function test_f {
+	function test_f {
 		for i ; do
 			print_testing_flac
 			ERROR="$((flac -ts "$i") 2>&1)"
@@ -378,22 +444,90 @@ function test_flacs {
 	# Run the above function with the configured threads (multithreaded)
 	find "$DIRECTORY" -name *.flac -print0 | xargs -0 -n 1 -P "$CORES" bash -c 'test_f "$@"' --
 
-    if [[ -f "$TEST_ERRORS" ]] ; then
-        echo -e "\n ${BOLD_RED}*${NORMAL} Errors found in some FLAC files, please check"
-        echo -e " ${BOLD_RED}*${NORMAL} \"$TEST_ERRORS\" for errors"
-        exit 0
-    fi
+	if [[ -f "$TEST_ERRORS" ]] ; then
+		echo -e "\n ${BOLD_RED}*${NORMAL} Errors found in some FLAC files, please check"
+		echo -e " ${BOLD_RED}*${NORMAL} \"$TEST_ERRORS\" for errors"
+		exit 0
+	fi
+}
+
+# Use auCDtect to check FLAC validity
+function aucdtect {
+	title_aucdtect_flac
+
+	FIND_FLACS="$(find "$DIRECTORY" -name "*.flac" -print)"
+	if [[ -z "$FIND_FLACS" ]] ; then
+		no_flacs
+		exit 0
+	fi
+
+	# Abort script and check for any errors thus far
+	function aucdtect_abort {
+		echo -e "\n ${BOLD_GREEN}*${NORMAL} Control-C received, exiting script..."
+		# Don't remove WAV files in case user has WAV files there purposefully
+		# The script cannot determine between existing and script-created WAV files
+		WAV_FILES="$(find "$DIRECTORY" -name *.wav -print)"
+		if [[ -f "$AUCDTECT_ERRORS" ]] ; then
+			echo -e "\n ${BOLD_RED}*${NORMAL} Some FLAC files may be lossy sourced, please check"
+			echo -e " ${BOLD_RED}*${NORMAL} \"$AUCDTECT_ERRORS\" for errors"
+		fi
+		if [[ -n "$WAV_FILES" ]] ; then
+			echo -e "\n ${BOLD_RED}*${NORMAL} There are some temporary WAV files leftover that"
+			echo -e " ${BOLD_RED}*${NORMAL} couldn't be deleted because of script interruption"
+			echo
+			echo -e " ${YELLOW}*${NORMAL} This script cannot determine between existing WAV files"
+			echo -e " ${YELLOW}*${NORMAL} and script-created files by design.  Please delete the"
+			echo -e " ${YELLOW}*${NORMAL} below files manually:"
+			# Find all WAV files in chosen directory to display for manual deletion
+			find "$DIRECTORY" -name *.wav -print | while read i ; do
+				echo -e " ${YELLOW}*${NORMAL}     $i"
+			done
+		fi
+		exit $?
+	}
+	
+	# Trap SIGINT (Control-C) to abort cleanly
+	trap aucdtect_abort SIGINT
+
+	function aucdtect_f {
+		for i ; do
+			print_aucdtect_flac
+			# Decompress FLAC to WAV so auCDtect can read the audio file
+			flac --totally-silent -d "$i"
+			# The actual auCDtect command with highest accuracy setting
+			AUCDTECT_CHECK="$("$AUCDTECT_COMMAND" -m0 "${i%.flac}.wav")"
+			ERROR="$(echo "$AUCDTECT_CHECK" | tail -n1)"
+			if [[ "$ERROR" != "This track looks like CDDA with probability 100%" ]] ; then
+				print_aucdtect_issue
+				echo -e "[[$i]]\n"  "$ERROR\n" >> "$AUCDTECT_ERRORS"
+			else
+				print_ok_flac
+			fi
+			# Remove temporary WAV file
+			rm "${i%.flac}.wav"
+		done
+	}
+	export -f aucdtect_f
+
+	# Run the above function with the configured threads (multithreaded)
+	find "$DIRECTORY" -name *.flac -print0 | xargs -0 -n 1 -P "$CORES" bash -c 'aucdtect_f "$@"' --
+
+	if [[ -f "$AUCDTECT_ERRORS" ]] ; then
+		echo -e "\n ${BOLD_RED}*${NORMAL} Some FLAC files may be lossy sourced, please check"
+		echo -e " ${BOLD_RED}*${NORMAL} \"$AUCDTECT_ERRORS\" for issues"
+		exit 0
+	fi
 }
 
 # Check for unset MD5 Signatures in FLAC files
 function md5_check {
 	title_md5check_flac
 
-    FIND_FLACS="$(find "$DIRECTORY" -name "*.flac" -print)"
-    if [[ -z "$FIND_FLACS" ]] ; then
+	FIND_FLACS="$(find "$DIRECTORY" -name "*.flac" -print)"
+	if [[ -z "$FIND_FLACS" ]] ; then
 		no_flacs
 		exit 0
-    fi
+	fi
 
 	# Abort script and check for any errors thus far
 	function md5_check_abort {
@@ -425,11 +559,11 @@ function md5_check {
 	# Run the above function with the configured threads (multithreaded)
 	find "$DIRECTORY" -name *.flac -print0 | xargs -0 -n 1 -P "$CORES" bash -c 'md5_c "$@"' --
 	
-    if [[ -f "$MD5_ERRORS" ]] ; then
-        echo -e "\n ${BOLD_RED}*${NORMAL} The MD5 Signature is unset for some FLAC files, please check"
-        echo -e " ${BOLD_RED}*${NORMAL} \"$MD5_ERRORS\" for details"
-        exit 0
-    fi  
+	if [[ -f "$MD5_ERRORS" ]] ; then
+		echo -e "\n ${BOLD_RED}*${NORMAL} The MD5 Signature is unset for some FLAC files, please check"
+		echo -e " ${BOLD_RED}*${NORMAL} \"$MD5_ERRORS\" for details"
+		exit 0
+	fi  
 }
 
 # Extract wanted FLAC metadata
@@ -475,11 +609,11 @@ export -f set_vorbis_tags
 function redo_tags {
 	title_analyze_tags
 
-    FIND_FLACS="$(find "$DIRECTORY" -name "*.flac" -print)"
-    if [[ -z "$FIND_FLACS" ]] ; then
+	FIND_FLACS="$(find "$DIRECTORY" -name "*.flac" -print)"
+	if [[ -z "$FIND_FLACS" ]] ; then
 		no_flacs
 		exit 0
-    fi
+	fi
 	
 	# Keep SIGINT from exiting the script (Can cause all tags
 	# to be lost if done when tags are being removed!)
@@ -553,7 +687,7 @@ function prune_flacs {
 # Display a lot of help
 function long_help {
 	cat << EOF
-  Usage: $0 Usage: $0 [OPTION] [OPTION]... [PATH_TO_FLAC(s)]
+  Usage: $0 [OPTION] [OPTION]... [PATH_TO_FLAC(s)]
   Options:
     -c, --compress          Compress the FLAC files with level 8 compression AND verify the
                             resultant files.  This option will add a tag to all successfully
@@ -575,6 +709,20 @@ function long_help {
 
                             As with the "--compress" option, this will produce an error log if
                             any FLAC files are found to be corrupt.
+
+    -a, --aucdtect          Uses the auCDtect program by Oleg Berngardt and Alexander Djourik to
+                            analyze FLAC files and check with fairly accurate precision whether
+                            the FLAC files are lossy sourced or not.  For example, an MP3 file
+                            converted to FLAC is no longer lossless therefore lossy sourced.
+
+                            While this program isn't foolproof, it gives a good idea which FLAC
+                            files will need further investigation (ie a spectrograph).  This program
+                            does not work on FLAC files which have a sample rate and bit depth more
+                            than a typical audio CD (16bit / 44.1kHz), and will skip files that have
+                            a higher bit depth and sample rate.
+
+                            If any files are found to not be perfect (100% CDDA), a log will be created
+                            with the questionable FLAC files recorded in it.
 
     -m, --md5check          Check the FLAC files for unset MD5 Signatures and log the output of
                             any unset signatures.  An unset MD5 signature doesn't necessarily mean
@@ -652,9 +800,9 @@ function long_help {
     # Clear excess tags from each FLAC file
     $0 --redo /some/path/to/files
 
-	# Compress FLAC files to level 8 compression and redo
-	# the FLAC tags without the warning/countdown
-	$0 -c -r -d /some/path/to/files
+    # Compress FLAC files to level 8 compression and redo
+    # the FLAC tags without the warning/countdown
+    $0 -c -r -d /some/path/to/files
 EOF
 }
 
@@ -665,6 +813,7 @@ function short_help {
 	echo "    -c, --compress"
 	echo "    -t, --test"
 	echo "    -m, --md5check"
+	echo "    -a, --aucdtect"
 	echo "    -p, --prune"
 	echo "    -r, --redo"
 	echo "    -n, --no-color"
@@ -712,6 +861,10 @@ while [[ "$#" -gt 1 ]] ; do
 			TEST="true"
 			shift
 			;;
+		--aucdtect|-a)
+			AUCDTECT="true"
+			shift
+			;;
 		--md5check|-m)
 			MD5CHECK="true"
 			shift
@@ -752,14 +905,14 @@ fi
 # If no arguments are made to the script show usage
 if [[ "$#" -eq 0 ]] ; then
 	short_help
-    exit 0
+	exit 0
 fi
 
 # Make sure compress and test aren't both specified
 if [[ "$COMPRESS" == "true" && "$TEST" == "true" ]] ; then
-    echo -e " ${BOLD_RED}*${NORMAL} Running both \"--compress\" and \"--test\" is redundant as \"--compress\""
-    echo -e " ${BOLD_RED}*${NORMAL} already tests the FLAC files while compressing them.  Please"
-    echo -e " ${BOLD_RED}*${NORMAL} choose one or the other."
+	echo -e " ${BOLD_RED}*${NORMAL} Running both \"--compress\" and \"--test\" is redundant as \"--compress\""
+	echo -e " ${BOLD_RED}*${NORMAL} already tests the FLAC files while compressing them.  Please"
+	echo -e " ${BOLD_RED}*${NORMAL} choose one or the other."
 	exit 0
 fi
 
@@ -769,7 +922,7 @@ fi
 # This must come before the other options in
 # order for it to take effect
 if [[ "$NO_COLOR" == "true" ]] ; then
-    BOLD_GREEN=""
+	BOLD_GREEN=""
 	BOLD_RED=""
 	NORMAL=""
 	YELLOW=""
@@ -792,6 +945,18 @@ fi
 
 if [[ "$MD5CHECK" == "true" ]] ; then
 	md5_check
+fi
+
+if [[ "$AUCDTECT" == "true" ]] ; then
+	# Check if auCDtect is found/installed
+	if [[ -f "$AUCDTECT_COMMAND" ]] ; then
+		aucdtect
+	else
+		echo -e " ${BOLD_RED}*${NORMAL} It appears auCDtect is not installed or you have not"
+		echo -e " ${BOLD_RED}*${NORMAL} configured this script to find it. Please verify you"
+		echo -e " ${BOLD_RED}*${NORMAL} have this program installed."
+		exit 1
+	fi
 fi
 
 if [[ "$REDO" == "true" ]] ; then
